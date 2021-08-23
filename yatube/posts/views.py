25d58bb07fa159a9187796672a922c53cc3c2b1b
Darya <lunaticmoony13@gmail.com>
2021-08-23 21:27:19 +0300

    Tests passedgit add .git add .

---
+++ yatube/posts/views.py
@@ -0,0 +1,26 @@
+from posts.models import Post, Group
+from django.shortcuts import render, get_object_or_404
+
+
+# Create your views here.
+def index(request):
+    template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    title = f'Записи сообщества {group.title}'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, template, context)
